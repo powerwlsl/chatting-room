0b9d7959761b89fc716aa0ccf379ac684bcb0432
Hyejin Ahn <thebestisyet2com@gmail.com>
2016-12-13 16:24:38 -0800

    Add route of chatrooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :chat_rooms
+  root to: "chat_rooms#index"
 end
