85f7baa3d6c3a3c703e99a34087d7b2052796946
Hyejin Ahn <thebestisyet2com@gmail.com>
2016-12-13 16:53:49 -0800

    Install redis gem

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -18,7 +18,7 @@ class ChatRoomsController < ApplicationController
   end
 
   def show 
-    @chat_room = ChatRoom.include(:messages).find_by(id: params[:id])
+    @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :chat_rooms
-  root to: "chat_rooms#index"
+  resources :chat_rooms, only: [:new,:create,:show,:index]
+  root 'chat_rooms#index'
+
+  mount ActionCable.server => '/cable'
 end
